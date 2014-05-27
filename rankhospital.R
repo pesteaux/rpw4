b10e32b3713d7b968cf8bdec3867284d0c39c717
pesto <pesto@tralfamador.home>
2014-05-26 23:54:03 -0500

    coffee shop update

--- rankhospital.R
+++ rankhospital.R
@@ -3,7 +3,8 @@ rankhospital <- function(state, outcome = c("pneumonia","heart attack","heart fa
   usage <- "Enter the two-letter abbreviation of a state (with no period) and an outcome of either \'pneumonia\', \'heart attack\' or \'heart failure\'."
 
   ## Read outcome data
-  setwd("U:/School/Coursera/R Programming/RPW4/rprog-data-ProgAssignment3-data")
+#  setwd("U:/School/Coursera/R Programming/RPW4/rprog-data-ProgAssignment3-data")
+  setwd("/Users/pesto/Documents/school/coursera/rprogramming")
   outcomeFile <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
   ## Check that state and outcome are valid
@@ -31,13 +32,18 @@ rankhospital <- function(state, outcome = c("pneumonia","heart attack","heart fa
     if(tolower(outcome)=="heart failure") {
       hfsub<-subset(outcomeFile,outcomeFile$State==state,select=c(Hospital.Name,State,Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))
       hfsub1<-subset(hfsub,hfsub[3] != "Not Available")
+#      print(class(hfsub1$rank))
       hfsub1[3]<-lapply(hfsub1[3],as.numeric)
-      order.hfsub1<-order(hfsub1[3])
+      print(class(hfsub1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))
+      hfsub1$comb<-paste(hfsub1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,hfsub1$Hospital.Name)
+      hfsub1$rank<-rank(hfsub1$comb)
+      order.hfsub1<-order(hfsub1[5])
       ordered.hfsub1<-hfsub1[order.hfsub1,]
-      lowesthf<-ordered.hfsub1[1,3]
-      allLowesthf<-subset(ordered.hfsub1,ordered.hfsub1[3]==lowesthf,select=c(Hospital.Name))
-      order.allLowesthf<-allLowesthf[order(allLowesthf$Hospital.Name),]
-      return(order.allLowesthf)
+#      lowesthf<-ordered.hfsub1[1,3]
+#      allLowesthf<-subset(ordered.hfsub1,ordered.hfsub1[3]==lowesthf,select=c(Hospital.Name))
+     print(head(ordered.hfsub1))      
+      hfresultdf<-subset(hfsub1,hfsub1$rank==num)
+      return(hfresultdf)
       
     }
   if(tolower(outcome)=="pneumonia") {
