fe8b700d151e8a5c50658cd391f1b72c3ceb6cfa
pesteaux <pesteaux@users.noreply.github.com>
2014-05-27 02:04:24 -0500

    Manual Update via github
    You need to change everything to look like heart failure. it works sorta.

--- rankhospital.R
+++ rankhospital.R
@@ -3,8 +3,8 @@ rankhospital <- function(state, outcome = c("pneumonia","heart attack","heart fa
   usage <- "Enter the two-letter abbreviation of a state (with no period) and an outcome of either \'pneumonia\', \'heart attack\' or \'heart failure\'."
 
   ## Read outcome data
-#  setwd("U:/School/Coursera/R Programming/RPW4/rprog-data-ProgAssignment3-data")
-  setwd("/Users/pesto/Documents/school/coursera/rprogramming")
+  setwd("U:/School/Coursera/R Programming/RPW4/rprog-data-ProgAssignment3-data/")
+#  setwd("/Users/pesto/Documents/school/coursera/rprogramming")
   outcomeFile <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
   ## Check that state and outcome are valid
@@ -34,16 +34,18 @@ rankhospital <- function(state, outcome = c("pneumonia","heart attack","heart fa
       hfsub1<-subset(hfsub,hfsub[3] != "Not Available")
 #      print(class(hfsub1$rank))
       hfsub1[3]<-lapply(hfsub1[3],as.numeric)
-      print(class(hfsub1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))
-      hfsub1$comb<-paste(hfsub1$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure,hfsub1$Hospital.Name)
-      hfsub1$rank<-rank(hfsub1$comb)
-      order.hfsub1<-order(hfsub1[5])
+      order.hfsub1<-order(hfsub1[3],hfsub1[1])
       ordered.hfsub1<-hfsub1[order.hfsub1,]
+      ranks<-data.frame(rank=1:nrow(ordered.hfsub1))
+      ordered.hfsub1<-cbind(ordered.hfsub1,ranks)
 #      lowesthf<-ordered.hfsub1[1,3]
 #      allLowesthf<-subset(ordered.hfsub1,ordered.hfsub1[3]==lowesthf,select=c(Hospital.Name))
-     print(head(ordered.hfsub1))      
-      hfresultdf<-subset(hfsub1,hfsub1$rank==num)
-      return(hfresultdf)
+  #   print(head(ordered.hfsub1))
+      if(num=="best") {getRank<-1}
+      else if(num=="worst") {getRank<-nrow(ordered.hfsub1)}
+      else {getRank<-num}
+      hfresultdf<-subset(ordered.hfsub1,ordered.hfsub1$rank==getRank)
+      return(hfresultdf[1.1])
       
     }
   if(tolower(outcome)=="pneumonia") {
